945cb5343b01b321ac88d8a923546f1f1854f603
BartoszPatro <161605900+BartoszPatro@users.noreply.github.com>
2024-05-08 13:23:59 +0000

    C_1
    C_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,18 +8,44 @@ Kod bazowy programu Commit4_0:
 */
 
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
+
+
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Wybierz opcję: ");
+      System.out.println("1. Dodaj studenta");
+      System.out.println("2.Wypisz wszystkich studentów");
+      System.out.println("3.Dodaj nowego studenta z konsoli z innymi danymi");
 
+      int opcja=scanner.nextInt();
+
+      switch(opcja)
+        {
+            case 1:
+            System.out.println("Podaj imię studenta: ");
+            String imie=scanner.nextLine();
+            System.out.println("Podaj nazwisko studenta: ");
+            String nazwisko=scanner.nextLine();
+            System.out.println("Podaj wiek studenta: ");
+            int wiek=scanner.nextInt();
+            System.out.println("Nowy student to:" + imie + " " + nazwisko+ " " + wiek );
+            s.addStudent(new Student(imie, wiek));
+
+
+
+
+
+        }
       var students = s.getStudents();
       for(Student current : students) {
         System.out.println(current.ToString());
       }
+      scanner.close();
     } catch (IOException e) {
 
     }
